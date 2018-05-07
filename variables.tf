508778475fe378da9132a008e81a1121002bd532
jeremy avnet <brainsik@truss.works>
2018-05-07 14:44:32 -0700

    Add VPC flow logs to app-environment module

--- variables.tf
+++ variables.tf
@@ -7,7 +7,7 @@ variable "environment" {
 }
 
 variable "logs_cloudwatch_retention" {
-  description = "Specifies the number of days you want to retain log events in the log group."
+  description = "Number of days you want to retain log events in the log group."
   default     = 90
 }
 
