41d96a618ffb0c3e17cf8f468eb7281822e4544e
Michael Kania <dynamike@truss.works>
2018-08-16 16:41:00 -0700

    add output for task_execution_role_arn

--- outputs.tf
+++ outputs.tf
@@ -3,6 +3,11 @@ output "ecs_security_group_id" {
   value       = "${aws_security_group.ecs_sg.id}"
 }
 
+output "task_execution_role_arn" {
+  description = "The ARN of the task execution role that the Amazon ECS container agent and the Docker daemon can assume."
+  value       = "${join("", aws_iam_role.task_execution_role.*.arn)}"
+}
+
 output "task_role_arn" {
   description = "The ARN of the IAM role assumed by Amazon ECS container tasks."
   value       = "${aws_iam_role.task_role.arn}"
