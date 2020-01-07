8a45d6f997abcd24384caf7632dfe1cae0db6517
kilbergr <kilbergr@users.noreply.github.com>
2020-01-07 13:29:11 -0500

    for both main and main_no_lb

--- main.tf
+++ main.tf
@@ -541,7 +541,7 @@ resource "aws_ecs_service" "main" {
   network_configuration {
     subnets          = var.ecs_subnet_ids
     security_groups  = [aws_security_group.ecs_sg.id]
-    assign_public_ip = false
+    assign_public_ip = var.assign_public_ipse
   }
 
   load_balancer {
