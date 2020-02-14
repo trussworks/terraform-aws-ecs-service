07de10f555628cf4bb3fd539544488cc8c13db7e
Lee Hicks <lee@truss.works>
2020-02-14 22:23:11 +0000

    Module output IAM role itself instead of name.

--- outputs.tf
+++ outputs.tf
@@ -18,6 +18,11 @@ output "task_role_name" {
   value       = aws_iam_role.task_role.name
 }
 
+output "task_role" {
+  description = "The IAM role assumed by Amazon ECS container tasks."
+  value       = aws_iam_role.task_role
+}
+
 output "task_definition_arn" {
   description = "Full ARN of the Task Definition (including both family and revision)."
   value       = aws_ecs_task_definition.main.arn
