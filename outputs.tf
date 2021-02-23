9adcec3b8d55fc129ba16165f40245c619f5972d
Steph Gosling <steph@chuci.org>
2021-02-23 13:30:38 +0000

    feature: support optional security group management

--- outputs.tf
+++ outputs.tf
@@ -1,6 +1,6 @@
 output "ecs_security_group_id" {
   description = "Security Group ID assigned to the ECS tasks."
-  value       = aws_security_group.ecs_sg.id
+  value       = join("", aws_security_group.ecs_sg.*.id)
 }
 
 output "task_execution_role_arn" {
